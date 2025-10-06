1c31eb053b1340bee7ae7b8fc40d96cc4076885c
David Tretter <d.tretter@freenet.de>
2025-10-06 11:06:44 +0200

    Added Form with validation

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,8 +6,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
 
   reservationForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      checkGuestName: ['', Validators.required],
+      checkGuestEmail: ['', [Validators.required, Validators.email]],
+      checkRoomNumber: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if(this.reservationForm.valid){
+      console.log("valid")
+    }
+  }
 
 }
